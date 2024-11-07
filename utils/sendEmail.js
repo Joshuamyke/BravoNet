afa3b57551d05b4e08dc48a13cfc49257fc76da0
Joshua Chinonyerem <omjoshuamike@gmail.com>
2024-11-07 14:45:35 +0100

    Sendmail updated

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,20 +1,25 @@
 const nodemailer = require('nodemailer');
 
 const sendEmail = async (email, subject, text) => {
-    const transporter = nodemailer.createTransport({
-        service: 'Gmail', // Bravo Email Services
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS,
-        },
-    });
+    try {
+        const transporter = nodemailer.createTransport({
+            service: 'Gmail', // Bravo Email Services
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS,
+            },
+        });
 
-    await transporter.sendMail({
-        from: process.env.EMAIL_USER,
-        to: email,
-        subject: subject,
-        text: text,
-    });
+        await transporter.sendMail({
+            from: process.env.EMAIL_USER,
+            to: email,
+            subject: subject,
+            text: text,
+        });
+        console.log('Email sent successfully');
+    } catch (error) {
+        console.error('Error sending email:', error);
+    }
 };
 
 module.exports = sendEmail;
